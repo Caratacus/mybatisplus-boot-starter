14004c5693430ff3e1a75cd0dd59621e57ecc8c9
Caratacus <330952486@qq.com>
2017-04-30 01:45:48 +0800

    Mybatis plus boot starter complete, Wait for release!

--- mybatisplus-spring-boot-starter/src/main/java/com/baomidou/mybatisplus/spring/boot/autoconfigure/ConfigurationCustomizer.java
+++ mybatisplus-spring-boot-starter/src/main/java/com/baomidou/mybatisplus/spring/boot/autoconfigure/ConfigurationCustomizer.java
@@ -12,6 +12,7 @@ public interface ConfigurationCustomizer {
 
     /**
      * Customize the given a {@link Configuration} object.
+     *
      * @param configuration the configuration object to customize
      */
     void customize(Configuration configuration);
